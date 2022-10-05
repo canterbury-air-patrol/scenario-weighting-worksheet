5767fd6a191be890b761e51d9641d9188c215237
Scott Parlane <scott@parlanenz.com>
2022-10-05 20:05:09 +1300

    Create scenario weighting worksheet
    Based on the nzsar Scenario Weighting Worksheet
    
    Supports as many scenarios and people as needed.
    Automatically calculates the relative weightings.

---
+++ sww.js
@@ -0,0 +1,9 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react'
+import * as ReactDOM from 'react-dom/client'
+
+import { ScenarioWeightingWorksheet } from './scenario-weighting-worksheet'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<ScenarioWeightingWorksheet />)

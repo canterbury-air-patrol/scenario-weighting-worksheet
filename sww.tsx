fa3608d39868e717bebc110563b5a8aefa2be7f6
Scott Parlane <scott@parlanenz.com>
2024-12-21 16:18:56 +1300

    Switch to typescript

--- sww.tsx
+++ sww.tsx
@@ -5,5 +5,5 @@ import * as ReactDOM from 'react-dom/client'
 
 import { ScenarioWeightingWorksheet } from './scenario-weighting-worksheet'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(<ScenarioWeightingWorksheet />)
